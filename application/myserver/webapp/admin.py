16d7d42f21aefde7873d5da2240e4782f92f746a
Tebogo Matenchi <tebogomatenchi@gmail.com>
2025-05-15 01:37:47 +0200

    Delete application directory

--- application/myserver/webapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Lecturer,Student,Admin,Subject,UserLogs,Department,Attendance,StudentProgress,FaceRecognition,LecturerCourses,Assessment,StudentMarks
-
-# Register your models here.
-admin.site.register((Lecturer,Student,Admin,Subject,UserLogs,Department,Attendance,StudentProgress,FaceRecognition,LecturerCourses,Assessment,StudentMarks))
